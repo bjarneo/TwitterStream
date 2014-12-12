37192d23b71f52fd4b6666c9713ed2ce13140fcb
bjarneo <bjarne.oeverli@gmail.com>
2014-12-12 14:18:55 +0100

    added index.html to public folder

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ twitter.on('tweet', function(tweet) {
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 http.listen(3000, function(){
