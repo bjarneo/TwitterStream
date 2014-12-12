dc38eb75ec5e450ecf84bec349f0a0255d61d657
bjarneo <bjarne.oeverli@gmail.com>
2014-12-12 14:14:59 +0100

    first

---
+++ index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var express = require('express'),
+    app = express(),
+    http = require('http').Server(app),
+    io = require('socket.io')(http),
+    config = require('./config.json'),
+    twitter = require('node-tweet-stream')(config);
+
+// What keywords to track
+[
+    'JavaSript',
+    'node.js',
+    'nodejs',
+    'python'
+].forEach(function(keyword) {
+    twitter.track(keyword);
+});
+
+twitter.on('tweet', function(tweet) {
+    io.emit('tweet', {
+        'user': {
+            'name': tweet.user.name,
+            'screenName': tweet.user.screen_name,
+            'backgroundColor': tweet.user.profile_background_color,
+            'profileImage': tweet.user.profile_image_url
+        },
+        'text': tweet.text,
+        'createdAt': tweet.created_at
+    });
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
---
+++ public/js/twitter-stream-app.js
@@ -0,0 +1,71 @@
+'use strict';
+
+var TwitterStreamItem = React.createClass({
+    render: function() {
+        var tweetNodes = this.props.data.map(function(tweet) {
+            var userColor = {
+                background: '#' + tweet.user.backgroundColor
+            };
+
+            return (
+                <div className="media msg">
+                    <span className="userColor" style={userColor}></span>
+                    <a className="pull-left user-image" href="#">
+                        <img className="media-object" src={tweet.user.profileImage} />
+                    </a>
+                    <div className="media-body">
+                        <small class="pull-right time">
+                            <i class="glyphicon glyphicon-time"></i> {tweet.createdAt}
+                        </small>
+                        <h5 className="media-heading">{tweet.user.name} ~ {tweet.user.screenName}</h5>
+                        <p class="col-lg-10">{tweet.text}</p>
+                    </div>
+                </div>
+            );
+        });
+
+        return (
+            <div className="twitter-item">{tweetNodes}</div>
+        );
+    }
+});
+
+var TwitterStreamList = React.createClass({
+    render: function() {
+        return (
+            <div className="twitter-list message-wrap col-lg-8">
+                <TwitterStreamItem data={this.props.data}></TwitterStreamItem>
+            </div>
+        );
+    }
+});
+
+var TwitterStreamApp = React.createClass({
+    getInitialState: function() {
+        return {
+            tweets: []
+        };
+    },
+
+    componentDidMount: function() {
+        var socket = io();
+
+        socket.on('tweet', function(tweet) {
+            var tweets = this.state.tweets.slice();
+
+            tweets.unshift(tweet);
+
+            this.setState({ tweets: tweets });
+        }.bind(this));
+    },
+
+    render: function() {
+        return (
+            <div className="TwitterStream">
+                <TwitterStreamList data={this.state.tweets}/>
+            </div>
+        );
+    }
+});
+
+React.render(<TwitterStreamApp />, document.getElementById('TwitterStreamWrapper'));
