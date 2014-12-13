109b0d2ee8770f7e635f4bc875d6b16783fc34d9
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2014-12-13 16:59:06 +0100

    spaces

--- index.js
+++ index.js
@@ -32,10 +32,10 @@ twitter.on('tweet', function(tweet) {
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-http.listen(3000, function(){
+http.listen(3000, function() {
     console.log('listening on *:3000');
 });
