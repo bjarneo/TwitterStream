f5cf17aa002af75c8d93ef9229350b74d5522a78
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2014-12-15 19:48:35 +0100

    no need for closure here

--- index.js
+++ index.js
@@ -40,6 +40,4 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-http.listen(3000, function() {
-    console.log('listening on *:3000');
-});
+http.listen(3000, console.log('listening on *:3000'));
